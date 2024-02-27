bb6c135e1c1b7edab9865694f6588eef945663c7
SnikiEP <71599139+SnikiEP@users.noreply.github.com>
2024-02-27 11:12:48 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  'db_node_cloud', // Database name
-  'db_node_cloud_user', // User
-  'CpuOIraLdNGRNBB1WGiLaMZNfm1M1DlA', // Password
+  'process.env.DB_NAME', // Database name
+  'process.env.DB_USER', // User
+  'process.env.DB_PASSWORD', // Password
   {
-    host: 'dpg-cneqqu6n7f5s73e2g45g-a', // Host
+    host: 'process.env.DB_HOST', // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
